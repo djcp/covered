a48489bf8c72ca0793c1fbe4d6670a97c366a360
djcp <dan@collispuro.net>
2012-04-02 16:53:10 -0400

    framework.

---
+++ public/javascripts/covered.js
@@ -0,0 +1,15 @@
+// Coming soon!
+//
+
+$.extend({
+apiEndpoint: function(){ return 'http://api.dp.la/dev/' },
+
+});
+
+$(document).ready(function(){
+  $('form#query').submit(function(e){
+    e.preventDefault();
+    $.ajax(
+  });
+});
+

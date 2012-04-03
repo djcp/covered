d4afb86ee776b3731eb544c9020faf5f8d82525d
djcp <dan@collispuro.com>
2012-04-03 09:42:31 -0400

    Filtering working.

--- public/javascripts/covered.js
+++ public/javascripts/covered.js
@@ -1,25 +1,30 @@
 $.extend({
   apiEndpoint: function(){ return 'http://api.dp.la/dev/' },
   constructDoc: function(d,facets){
-    var node = $('<div />').attr({class: 'doc ' + d.data_source, id: 'doc-' + d.id}).html(d.title);
-    $(node).append($('<span />').attr({class: 'data_source'}).html(d.data_source));
-    facets[d.data_source] = 1;
+    var node = $('<div />').attr({class: 'doc ' + d.data_source, id: 'doc-' + d.id});
     if(d.id_isbn != undefined){
       $(d.id_isbn).each(function(i,isbn){
         $(node).append($('<img />').attr({src: 'http://covers.openlibrary.org/b/isbn/' + isbn + '-S.jpg', class: 'cover'}));
       });
     }
+    $(node).append(d.title);
+    $(node).append($('<span />').attr({class: 'data_source'}).html(d.data_source));
+    facets[d.data_source] = 1;
     return node;
   },
   initIsotope: function(){
-    $('#target').isotope({
+    var isotope_obj = $('#target').isotope({
       itemSelector : '.doc',
       layoutMode : 'fitRows'
+    });
+    $('.filter').click(function(e){
+      $(isotope_obj).isotope({filter: $(this).attr('data_filter_class')});
     });
   }
 });
 
 $(document).ready(function(){
+  $('#keyword').focus();
   $('form#query').submit(function(e){
     e.preventDefault();
     $.ajax({
@@ -43,8 +48,9 @@ $(document).ready(function(){
           $('#target').append($.constructDoc(el,facets));
         });
         $.each(facets, function(key,val){
-          $('#facets').append(key);
+          $('#facets').append($('<span/>').attr({class: 'filter', data_filter_class: "." + key}).html(key));
         });
+        $('#facets').append($('<span/>').attr({class: 'filter', data_filter_class: '*'}).html('Show all'));
       }
 
     });

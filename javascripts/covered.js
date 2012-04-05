4845aee8fcbd6c5161129b4bb9f89e3b738f2617
ao5357 <ao5357@gmail.com>
2012-04-05 11:17:34 -0400

    relatedEditions pulls xISBN

--- javascripts/covered.js
+++ javascripts/covered.js
@@ -6,15 +6,29 @@ $.extend({
     var node = $('<div />').attr({class: 'doc ' + d.data_source, id: 'doc-' + d.id});
     if(d.id_isbn != undefined){
       $(d.id_isbn).each(function(i,isbn){
+      	// var related = $.relatedEditions(isbn);
         $(node).append($('<img />').attr({src: 'http://covers.openlibrary.org/b/isbn/' + isbn + '-S.jpg', class: 'cover'}));
-      });
+      	});
     }
     $(node).append(d.title);
     $(node).append($('<span />').attr({class: 'data_source'}).html(d.data_source));
     facets[d.data_source] = (facets[d.data_source] == undefined) ? 1 : (facets[d.data_source] + 1);
     return node;
   },
-
+	
+	relatedEditions: function(isbn){
+		var related = [];
+		$.getJSON('http://xisbn.worldcat.org/webservices/xid/isbn/' + isbn + '?method=getEditionsa&format=json&fl=*&callback=?')
+			.done(function(data){
+				if(data.list.length >= 1){
+					$.each(data.list,function(i,record){
+						related.push(record.isbn[0]);
+						});
+					}
+				});
+		return related;
+		},
+	
   initIsotope: function(){
     var isotope_obj = $('#target').isotope({
       columnWidth: 300,
